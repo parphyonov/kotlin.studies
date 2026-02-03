e76af8efe5857070192184040ed0f671ed180541
Alexei Parphyonov <alexeiparphyonov@gmail.com>
2026-02-03 20:14:40 +0300

    solved Config.kt from 4.2

--- src/kontrolnye/Config.kt
+++ src/kontrolnye/Config.kt
@@ -1,18 +1,65 @@
-package testworks
+package kontrolnye
+
+const val DEFAULT_TIMEOUT = 30
+const val TIMEOUT_LOWER_BOUNDARY = 1
+const val TIMEOUT_UPPER_BOUNDARY = 300
+
+const val DEFAULT_MAX_RETRIES = 3
 
 enum class LogLevel {
-        TRACE, DEBUG, INFO, WARN, ERROR, FATAL
+    TRACE, DEBUG, INFO, WARN, ERROR, FATAL
 }
 
-class Config(
-) {
-    var timeout: Int = 30
+class Config {
+    var timeout: Int = DEFAULT_TIMEOUT
         set(value) {
-            if (value !in 1..300) {
-                println("Ошибка: Время ожидания должно быть в диапазоне от 1 до 300 секунд. Установлено значение по умолчанию.")
-                field = 30
+            if (value !in TIMEOUT_LOWER_BOUNDARY..TIMEOUT_UPPER_BOUNDARY) {
+                val formattedMsg = "Время ожидания должно быть в диапазоне от $TIMEOUT_LOWER_BOUNDARY " +
+                    "до $TIMEOUT_UPPER_BOUNDARY секунд"
+                defaultSettingDisclaimer(formattedMsg)
+                field = DEFAULT_TIMEOUT
             } else {
                 field = value
             }
         }
+
+    var maxRetries: Int = DEFAULT_MAX_RETRIES
+        set(value) {
+            if (value < 0) {
+                defaultSettingDisclaimer("Максимальное количество повторных попыток не может быть отрицательным")
+                field = DEFAULT_MAX_RETRIES
+            } else {
+                field = value
+            }
+        }
+
+    var loggingLevel: LogLevel = LogLevel.INFO
+        set(value) {
+            when (value) {
+                LogLevel.TRACE, LogLevel.FATAL -> {
+                    println("Ошибка: Уровень $value недоступен.")
+                    field = field
+                }
+
+                else -> field = value
+            }
+        }
+
+    val isDebugMode: Boolean
+        get() = loggingLevel == LogLevel.DEBUG
+
+    val isProductionMode: Boolean
+        get() = loggingLevel == LogLevel.ERROR
+
+    fun printConfig() {
+        println("Время ожидания: $timeout секунд")
+        println("Максимальное количество повторных попыток: $maxRetries")
+        println("Уровень логирования: ${loggingLevel.name}")
+        println("Режим отладки: $isDebugMode")
+        println("Режим продакшн: $isProductionMode")
+    }
+
+    fun defaultSettingDisclaimer(message: String) {
+        print("Ошибка: $message. Установлено значение по умолчанию.")
+    }
 }
